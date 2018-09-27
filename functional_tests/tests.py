34fe751f8886be4a3d94afe0aa333fc47fa026c8
Rashedul Kabir <rkabir.rashed@gmail.com>
2018-09-27 11:29:19 +0600

    Hack FT to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,21 @@
 import time
-import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+import os
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
@@ -56,7 +59,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
 
@@ -99,4 +101,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    # def test_layout_and_styling(self):
+    #     self.browser.get(self.live_server_url)
+    #     self.browser.set_window_size(1024, 768)
+    #
+    #     # She notices the input box is nicely centered
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertAlmostEqual(
+    #         inputbox.location['x'] + inputbox.size['width'] / 2,
+    #         512,
+    #         delta=10
+    #     )
 
